d56b44694202b87d6656624519d95705a5c0c2bc
Chidimma39 <alorgrace35@gmail.com>
2022-12-31 20:53:55 -0800

    combination of single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print the possible combination of single digits
+ * Deacription: using putchar function
+ * Return: Always 0
+ **/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
